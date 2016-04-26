7dcfc8a20a34fd7169972f4d936eaca1561835c8
Ryan Strat <ryanstrat@users.noreply.github.com>
2016-04-25 22:51:12 -0400

    completed ability to get ratings
    Endpoint allows filtering by email, major, and movie

--- src/routes/ratings.php
+++ src/routes/ratings.php
@@ -1,5 +1,15 @@
 <?php
 
+function addConjunction($text, $position) {
+	if($position == 1) {
+		return $text." WHERE";
+	} else if ($position > 1) {
+		return $text." AND";
+	} else {
+		return;
+	}
+}
+
 $app->post('/api/rating', function ($request, $response, $args) {
 	$params = $request->getParsedBody();
 
@@ -15,9 +25,9 @@ $app->post('/api/rating', function ($request, $response, $args) {
 	$this->logger->info("stars: ". $stars);
 	$this->logger->info("review: ". $review);
 
-	if (strlen($imdbid) > 9) {
+	if (strlen($imdbid) != 9) {
 		//bad input reply with 400:Bad Request
-		return $response.withStatus(400);
+		return $response->withStatus(400);
 	}
 
 	$accountID = get_account_id_from_email($this, $email);
@@ -38,3 +48,64 @@ $app->post('/api/rating', function ($request, $response, $args) {
 	return $response->withHeader('Content-Type', 'application/json')->write(json_encode($data));
 
 });
+
+$app->get('/api/rating', function($request, $response, $args) {
+	$this->logger->info("GET /api/rating");
+
+	$params = $request->getQueryParams();
+
+	$query = "SELECT movie, stars, review, email, major FROM ratings INNER JOIN users";
+	$query = $query . " ON ratings.user=users.account_id";
+	$query = $query . " INNER JOIN accounts ON users.account_id=accounts.id";
+
+	$filters = 0;
+	$SQLformat = "";
+
+
+	if (isset($params['email'])) {
+		$email = $params['email'];
+		$filters += 1;
+		
+		$query = addConjunction($query, $filters);
+
+		$query = $query . " email=?";
+		$SQLformat = $SQLformat . "s";
+		$SQLparams[] = $email;
+	}
+
+	if (isset($params['major'])) {
+		$major = $params['major'];
+		$filters += 1;
+		
+		$query = addConjunction($query, $filters);
+
+		$query = $query . " major=?";
+		$SQLformat = $SQLformat . "s";
+		$SQLparams[] = $major;
+	}
+
+	if (isset($params['movie'])) {
+		$movie = $params['movie'];
+		$filters += 1;
+		
+		$query = addConjunction($query, $filters);
+
+		$query = $query . " movie=?";
+		$SQLformat = $SQLformat . "s";
+		$SQLparams[] = $movie;
+	}
+
+	$this->logger->info("SQL Query: ". $query);
+	
+	$link = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+	if ($filters == 0) {
+		$result = mysqli_prepared_query($this, $link, $query);
+	} else {
+		$result = mysqli_prepared_query($this, $link, $query, $SQLformat, $SQLparams);
+	}
+	mysqli_close($link);
+
+	return $response->withHeader('Content-Type', 'application/json')->write(json_encode($result));
+
+});
+
